af1aaba826d8782ebeb9d7c9cf1c9cd2ac4c0ad2
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-31 12:58:37 +0530

    Fixing For Deployment

--- Backend/src/main/java/Linkly/backend/Backend/BackendApplication.java
+++ Backend/src/main/java/Linkly/backend/Backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package Linkly.backend.Backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void checkEnv() {
+		System.out.println("DB_URL -> " + System.getenv("DB_URL"));
+		System.out.println("DB_USER -> " + System.getenv("DB_USER"));
+		System.out.println("DB_PASSWORD -> " + System.getenv("DB_PASSWORD"));
 	}
 
 }
