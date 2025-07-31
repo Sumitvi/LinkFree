f1ef5093ae3e35117407d4725030fcd710cd7553
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-31 13:07:25 +0530

    Fixing Bugs

--- Backend/src/main/java/Linkly/backend/Backend/BackendApplication.java
+++ Backend/src/main/java/Linkly/backend/Backend/BackendApplication.java
@@ -8,14 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
 
-	@PostConstruct
-	public void checkEnv() {
+		SpringApplication.run(BackendApplication.class, args);
+
 		System.out.println("DB_URL -> " + System.getenv("DB_URL"));
 		System.out.println("DB_USER -> " + System.getenv("DB_USER"));
 		System.out.println("DB_PASSWORD -> " + System.getenv("DB_PASSWORD"));
 	}
+
 
 }
