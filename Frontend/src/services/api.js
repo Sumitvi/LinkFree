e1ee2b0f33dc2cac49c8599bc822f9c673ff2b19
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-31 14:29:07 +0530

    Fixing Frontend Url's

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://linkfree-huz0.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
 // ✅ Set default headers when token is available
